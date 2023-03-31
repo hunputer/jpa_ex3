a333ea0fcaa3ce839c384a0ed45555501494bf88
hunputer <wlgns3855@gmail.com>
2023-03-31 19:25:50 +0900

    commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,11 @@
+package hellojpa;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+    }
+}
